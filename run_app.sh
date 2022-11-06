5164cf57043ca06965f53324ccce45a639434a31
Mark Krautheim <mark.krautheim@gmail.com>
2022-11-06 12:15:47 +0000

    Change port mapping

--- run_app.sh
+++ run_app.sh
@@ -6,6 +6,6 @@ docker run --rm \
   -v $SCRIPT_PATH/data:/data \
   -v $SCRIPT_PATH/src:/app \
   -v /home/pi/ctreader-docker/data:/ctdata \
-  -p 192.168.1.9:8050:8050 \
+  -p 8050:8050 \
   dashmonitor
 
