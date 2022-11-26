5f00c6840afbd7fd84bc09b861d54c9fb6ffb916
Mark Krautheim <mark.krautheim@gmail.com>
2022-11-26 12:57:13 +0000

    Add volumes for development

--- run_bash.sh
+++ run_bash.sh
@@ -5,7 +5,9 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 docker run --rm -it \
   -v $SCRIPT_PATH/data:/data \
   -v $SCRIPT_PATH/src:/app \
+  -v $SCRIPT_PATH/ipython:/root/.ipython \
   -v /home/pi/ctreader-docker/data:/ctdata \
-  -p 8050:8050 \
-  allinone-py311 /bin/bash
+  -v /home/pi/analyze/data:/analyze \
+  allinone-py311 /bin/bash --rcfile /bashrc
+  #-p 8050:8050 \
 
